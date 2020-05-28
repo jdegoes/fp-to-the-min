b97376bbc133df161590991f8cbf0415e99eb89f
John A. De Goes <john@degoes.net>
2020-05-28 07:43:58 -0400

    Improve

--- fpmin/shared/src/main/scala/fpmin/Github.scala
+++ fpmin/shared/src/main/scala/fpmin/Github.scala
@@ -8,7 +8,13 @@ class Github() {
    * Downloads a file from Github.
    */
   def unsafeDownload(slug: String, file: String): String =
-    Source.fromURL(Github.downloadUrl(slug, file)).getLines().mkString("\n")
+    unsafeOpen(slug, file).getLines().mkString("\n")
+
+  /**
+   * Opens a file from Github, so that it can be streamed or downloaded.
+   */
+  def unsafeOpen(slug: String, file: String): Source =
+    Source.fromURL(Github.downloadUrl(slug, file))
 }
 object Github {
   private def downloadUrl(slug: String, file: String): String =
